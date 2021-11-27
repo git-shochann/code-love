6b1d1cd3cfa4904a60384e9f00ae047fbcd9f4a8
git-shochann <sho_825.sa216@docomo.ne.jp>
2021-11-28 05:25:36 +0900

    ♻️: refactoring, add webhook.json

--- puppeteer/search.js
+++ puppeteer/search.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-const profile = JSON.parse(fs.readFileSync('profile.json')); // ファイルの読み込みを非同期的にして、JSONでパースする
+const profile = JSON.parse(fs.readFileSync('puppeteer/profile.json')); // ファイルの読み込みを非同期的にして、JSONでパースする
 // profile.billing.lastName -> "Shoko"
 
 // const proxy = JSON.parse(fs.readFileSync('proxy.json')); // ファイルの読み込みを非同期的にして、JSONでパースする
@@ -12,6 +12,7 @@ const product_url =
 
 // seleniumの立ち上げ
 async function givePage() {
+  console.log('initilizing');
   const browser = await puppeteer.launch({
     headless: false,
     // args: ['--proxy-server=52.193.255.163:3128:sho:sho'],
@@ -23,6 +24,7 @@ async function givePage() {
 
 // 実際に商品ページに行き, カートに入れる
 async function addToCart(page) {
+  console.log('adding to cart');
   await page.goto(product_url);
   await page.waitForSelector('#noTradeIn');
   await page.click('#noTradeIn', (elem) => elem.click());
@@ -34,19 +36,27 @@ async function addToCart(page) {
   // await page.waitForNavigation(); // 違いは？
   await page.waitForSelector("button[data-autom='checkout']");
   await page.click("button[data-autom='checkout']", (elem) => elem.click());
+}
+
+//サインイン方法の選択 -> 今回はゲスト購入
+async function signIn(page) {
+  console.log('selecting sign-in method');
   await page.waitForNavigation();
   // await page.waitForSelector("button[data-autom='guest-checkout-btn']"); waitForNavigationであればいけた。
   await page.click("button[data-autom='guest-checkout-btn']", (elem) => elem.click());
+}
+
+// 受け取り方法の選択
+async function howToRecive(page) {
+  console.log('selecting method to receive');
   await page.waitForNavigation();
-  // ご希望の受け取り方法は？
-  await page.click('#fulfillmentOptionButtonGroup0', (elem) => elem.click());
+  await page.click("input[data-autom='fulfillment-option-HOME']", (elem) => elem.click());
   await page.waitForNavigation();
   await page.click("button[data-autom='checkout-zipcode-edit']", (elem) => elem.click());
   const input = await page.$("input[data-autom='form-field-postalCode']");
   await input.click({ clickCount: 3 });
   await input.type(profile.billing.zipCode, { delay: 300 });
   await page.select('select[data-autom=form-field-state]', profile.billing.prefecture);
-  // await page.waitForSelector("button[data-autom='fulfillment-continue-button']");
   await page.waitForNavigation();
   await page.click("button[data-autom='fulfillment-continue-button']", (elem) => elem.click());
   console.log('done');
@@ -54,6 +64,7 @@ async function addToCart(page) {
 
 // 住所を入力する
 async function fillBiling(page) {
+  console.log('filling billing');
   await page.waitForNavigation();
   await page.type("input[data-autom='form-field-lastName']", profile.billing.lastName);
   await page.type("input[data-autom='form-field-firstName']", profile.billing.firstName);
@@ -66,9 +77,14 @@ async function fillBiling(page) {
   await page.click("button[data-autom='shipping-continue-button']", (elem) => elem.click());
 }
 
+// discordにWebhookとして飛ばす
+async function sendWebhook(page) {}
+
 async function checkOut(page) {
   var page = await givePage();
   await addToCart(page);
+  await signIn(page);
+  await howToRecive(page);
   await fillBiling(page);
 }
 
