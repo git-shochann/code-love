daff6a2cdc42f972e4ece3e63b3d8bfde3a24e6c
git-shochann <sho_825.sa216@docomo.ne.jp>
2021-12-05 23:50:47 +0900

    🚀: add puppeteer, change README.md

--- 001/disney.js
+++ 001/disney.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -11,21 +12,53 @@ const cheerio = require('cheerio');
 const productUrl =
   'https://reserve.tokyodisneyresort.jp/ticket/search/?outside=1&route=2&parkTicketGroupCd=020&useDateFrom=20211219';
 
-// Tips : リクエストヘッダーをブラウザと一緒にすることで、Program判定を受けない
-
-const Monitor = async () => {
-  const htmlResponse = await axios(productUrl);
-  // 戻り値はHTMLなので、使用するBodyのみJSで使えるようにパースする
-  const $ = cheerio.load(htmlResponse.data);
-  // const available = $('.ticket-price-box').length;
-
-  // What is this?
-  await new Promise((r) => setTimeout(r, 3000));
-  Monitor();
-  return false;
+const Page = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  return page;
 };
 
-Monitor();
+const main = async () => {
+  const page = await Page();
+  await page.goto(productUrl);
+  console.log('page loaded');
+  await page.waitForSelector('.list-1day-02');
+  const disneySea = await page.$('.list-1day-02');
+  await disneySea.click();
+  console.log('disneySea clicked');
+
+  // テキストを取得する
+  // evaluateメソッドの第一引数に, アロー関数を設定する
+  const res = await page.evaluate(() => {
+    const element = document.getElementsByClassName('list-salesform-eticket-message');
+    const text = element[0].innerText;
+  });
+
+  console.log(res);
+
+  // await page.waitForSelector("button[data-role='none']");
+  // const printOutAtHome = await page.$("button[data-role='none']");
+  // await printOutAtHome.click();
+  // console.log('printOutAtHome clicked');
+};
+
+// Tips : リクエストヘッダーをブラウザと一緒にすることで、Program判定を受けない
+
+// const Monitor = async () => {
+//   const htmlResponse = await axios(productUrl);
+//   // 戻り値はHTMLなので、使用するBodyのみJSで使えるようにパースする
+//   const $ = cheerio.load(htmlResponse.data);
+//   // const available = $('.ticket-price-box').length;
+
+//   // What is this?
+//   await new Promise((r) => setTimeout(r, 3000));
+//   Monitor();
+//   return false;
+// };
+
+main();
 
 // // list-1day-01
 // const tdl = await page.$('.list-1day-01');
